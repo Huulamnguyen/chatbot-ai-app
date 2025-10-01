a5852838c5d3f8c9a736f3f5aff8ab936a220341
Huulamnguyen <liamnguyen.swe@gmail.com>
2025-09-30 21:29:11 -0400

    Setup TailwindCSS

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -12,7 +12,9 @@ function App() {
 
   return (
     <div>
-      <h1>{message || "Loading..."}</h1>
+      <h1 className="text-3xl font-bold text-cyan-400 text-center p-4">
+        {message || "Loading..."}
+      </h1>
     </div>
   );
 }
