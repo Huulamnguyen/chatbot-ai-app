9e53445bb88d7aff56eb95a3600bc4bfc8f16304
Huulamnguyen <liamnguyen.swe@gmail.com>
2025-09-30 22:08:18 -0400

    Setup Prettier

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,24 +1,24 @@
-import { useEffect, useState } from "react";
-import { Button } from "./components/ui/button";
+import { useEffect, useState } from 'react';
+import { Button } from './components/ui/button';
 
 function App() {
-  const [message, setMessage] = useState("");
+   const [message, setMessage] = useState('');
 
-  useEffect(() => {
-    fetch("/api/hello")
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message))
-      .catch((err) => console.error("Error fetching message:", err));
-  }, []);
+   useEffect(() => {
+      fetch('/api/hello')
+         .then((res) => res.json())
+         .then((data) => setMessage(data.message))
+         .catch((err) => console.error('Error fetching message:', err));
+   }, []);
 
-  return (
-    <div>
-      <h1 className="text-3xl font-bold text-cyan-400 text-center p-4">
-        {message || "Loading..."}
-      </h1>
-      <Button className="mx-auto block">Click Me</Button>
-    </div>
-  );
+   return (
+      <div>
+         <h1 className="text-3xl font-bold text-cyan-400 text-center p-4">
+            {message || 'Loading...'}
+         </h1>
+         <Button className="mx-auto block">Click Me</Button>
+      </div>
+   );
 }
 
 export default App;
